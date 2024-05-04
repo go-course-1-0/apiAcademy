8d5a3093a973528f411dae7fae64e5dc0f3113b9
Siyovush <paraparadox@protonmail.com>
2024-05-04 18:36:28 +0500

    Move connection to DB to separate func

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -1,36 +1,26 @@
 package main
 
 import (
+	"apiAcademy/internal/database"
 	"apiAcademy/internal/examples"
-	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 	"log"
 )
 
 const (
 	host     = "localhost" // 127.0.0.1
-	port     = 5432
+	port     = "5432"
 	user     = "postgres"
 	password = "postgres"
 	dbname   = "academy_db"
 )
 
 func main() {
-	// DSN - data source name
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := database.UsualConnect(host, port, user, password, dbname)
 	if err != nil {
-		log.Fatal("cannot connect to db:", err.Error())
+		log.Fatal("cannot connect to DB:", err.Error())
 	}
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("cannot ping db:", err.Error())
-	}
-
-	log.Println("Successfully connected!")
 
 	//examples.Example1(db)
 	//examples.Example2SqlInjection(db)
